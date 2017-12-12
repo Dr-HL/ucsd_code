193cfb12b5da11c0bcc68560a6643e9c9a77e084
Tim Baker <tim.baker@tempus.com>
2017-12-12 13:51:37 -0600

    completed code challenge 2

---
+++ code_chal_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+FrequentWordsProblem(Text,k)
+counts ← 0 for all possible kmers
+for i ← 0 to |Text| - |Pattern|
+kmer ← Text(i,|Pattern|)
+counts(kmer) ← counts(kmer) + 1
+count ← count + 1
+return all kmers where counts(kmer) = max(counts)
+
+12
+"""
+
+#fw_file = input('Enter file: ')
+fw_file = 'fqword.txt'
+def open_file(fw_file):
+    with open(fw_file, 'r') as f:
+        read_file = f.read().rstrip()
+        return read_file
+
+def frequent_words(text, k):
+    count = {}
+    for i in range(len(text)-k+1): #why +1?
+        if text[i:i+k] not in count:
+            count[text[i:i+k]] = 0
+        count[text[i:i+k]] += 1
+    m = max(count.values())
+    out = []
+    for kmer in count:
+        if count[kmer] == m:
+            out.append(kmer)
+    return out
+
+text = open_file(fw_file)
+k = 11
+print(frequent_words(text, k))
